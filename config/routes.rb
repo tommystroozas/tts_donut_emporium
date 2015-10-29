764a87ac797fd6449f331f48ce66891244f24c9b
Patrice <patricetaylor61@gmail.com>
2015-10-28 22:15:12 -0400

    donut_emporium

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :donuts
-
+  get "/donut_emporium" => "donuts#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
